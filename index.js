ae885bd222aeeeb78ff3bf68f98cd72910c15f54
Anastasiya Kesyan <kesyan_anastasiya@mail.ru>
2024-02-09 15:57:13 +0300

    created function digestFile

--- index.js
+++ index.js
@@ -16,3 +16,21 @@ async function topics() {
   }
   return './topics/otter_flashcard_data.txt';
 }
+
+// Обработка принятого файла, возвращаем массив объектов
+async function digestFile(path) {
+  const arrStr = (await fs.readFile(path, 'utf-8')).split('\r\n');
+  const arrQuestion = arrStr.filter((el, i) => i % 3 === 0);
+  const arrAnswer = arrStr
+    .filter((el, i) => i % 3 !== 0 && el !== '')
+    .map((el) => el.split(', '));
+  const arrObj = [];
+  for (let i = 0; i < arrQuestion.length; i += 1) {
+    arrObj.push({
+      question: arrQuestion[i],
+      choices: arrAnswer[i],
+      correctAnswer: arrAnswer[i][0],
+    });
+  }
+  // promtO(arrObj);
+}
